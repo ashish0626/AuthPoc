d4403fbdb26acbccd4b2b8a41ce04cde2854fd24
Ashishkumar Mangaldas Chourasia <ashishkumar.mangaldas-chourasia@hpe.com>
2021-12-15 14:27:05 +0530

    Role In capitial missed

--- Models/UserConstants.cs
+++ Models/UserConstants.cs
@@ -8,7 +8,7 @@ namespace JwtTokenPoc.Models
         {
             new UserModel() { Username = "hpe", EmailAddress = "hpe.admin@email.com", Password = "hpe", GivenName = "hpe", Surname = "hpe", Role = "Administrator" },
             new UserModel() { Username = "ashish", EmailAddress = "ashish.admin@email.com", Password = "ashish", GivenName = "ashish", Surname = "chourasia", Role = "Administrator" },
-            new UserModel() { Username = "developers", EmailAddress = "developers.developers@email.com", Password = "developers", GivenName = "developers", Surname = "developers", Role = "developers" },
+            new UserModel() { Username = "developers", EmailAddress = "developers.developers@email.com", Password = "developers", GivenName = "developers", Surname = "developers", Role = "Developers" },
         };
     }
 }
